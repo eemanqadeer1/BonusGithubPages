6f129a35db738e0530d1723ac76f826fced5a874
eeman <eeman.qadeer@gmail.com>
2022-11-24 06:45:08 +0500

    deoployment 0.0.1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import { Products, Navbar } from './Components/exports.js'
 import Axios from 'axios';
-
+import { products,cart } from './MockData.js';
 
 const App = () => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        Axios.get("http://localhost:5001/products")
-            .then((response) => {
-                setProducts(response.data);
-            })
-    }, [])
+    // const [products, setProducts] = useState([]);
+    // useEffect(() => {
+    //     Axios.get("http://localhost:5001/products")
+    //         .then((response) => {
+    //             setProducts(response.data);
+    //         })
+    // }, [])
 
-    const [cart, setCart] = useState([]);
+    // const [cart, setCart] = useState([]);
 
-    useEffect(() => {
-        Axios.get("http://localhost:5001/cart")
-            .then((response) => {
-                setCart(response.data);
-                // console.log(cart)
-            })
-    })
+    // useEffect(() => {
+    //     Axios.get("http://localhost:5001/cart")
+    //         .then((response) => {
+    //             setCart(response.data);
+    //             // console.log(cart)
+    //         })
+    // })
 
+   
     return (
         <div>
             <Navbar />
